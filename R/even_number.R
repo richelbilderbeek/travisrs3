e6d25869df15aa9c101fb18be55c261f02ccf700
richelbilderbeek <richel@richelbilderbeek.nl>
2019-11-17 12:13:43 +0100

    Stops if constructed with odd value

--- R/even_number.R
+++ R/even_number.R
@@ -1,8 +1,12 @@
 #' Create an even number
+#' @param value value for the even number
 #' @export
-even_number <- function(x) {
+even_number <- function(value) {
+  if (!(value %% 2 == 0)) {
+    stop("'value' must be an even number")
+  }
   structure(
-    list(x),
+    list(value),
     class = "even_number"
-  ) 
+  )
 }
--- tests/testthat/test-even_number.R
+++ tests/testthat/test-even_number.R
@@ -2,9 +2,8 @@ test_that("construction", {
   expect_silent(
     even_number(2)
   )
-  skip("WIP")
   expect_error(
     even_number(1),
-    "not even"
+    "'value' must be an even number"
   )
 })
