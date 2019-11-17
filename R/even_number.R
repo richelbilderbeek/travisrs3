f76d3bf7da78620973f661dab42224e848e7feb8
richelbilderbeek <richel@richelbilderbeek.nl>
2019-11-17 12:11:24 +0100

    Can construct an even number

---
+++ R/even_number.R
@@ -0,0 +1,8 @@
+#' Create an even number
+#' @export
+even_number <- function(x) {
+  structure(
+    list(x),
+    class = "even_number"
+  ) 
+}
--- tests/testthat/test-even_number.R
+++ tests/testthat/test-even_number.R
@@ -1,8 +1,8 @@
 test_that("construction", {
-  skip("WIP")
   expect_silent(
     even_number(2)
   )
+  skip("WIP")
   expect_error(
     even_number(1),
     "not even"
