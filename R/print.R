0d6e3b6f02b5572560128fdb02c0c55e425b93eb
richelbilderbeek <richel@richelbilderbeek.nl>
2019-11-28 17:03:22 +0100

    EOD

--- R/print.R
+++ R/print.R
@@ -1,6 +1,23 @@
-#' Print an even number
-#' @param even_number an even number of S3 class \link{even_number}
+#' Allow 'print.even_number' to be discovered
+#' @param value the object to be printed
+#' @param ... whatever
 #' @export
-print.even_number <- function(even_number) {
-  print(as.numeric(even_number))
+print <- function(value, ...){
+  UseMethod("print")
 }
+
+#' Print an even number
+#' @param value an even number of S3 class \link{even_number}
+#' @method print even_number
+#' @export
+print.even_number <- function(value) {
+  print(as.numeric(value))
+}
+
+#' #' Default print function
+#' #' @param value the object to be printed
+#' #' @param ... whatever
+#' #' @export
+#' print.default <- function(value, ...) {
+#'   print(value)
+#' }
